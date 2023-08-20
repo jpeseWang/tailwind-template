e680b92d618ea43c3808958284656f73cbb44f18
wang <quang07112004@gmail.com>
2023-08-20 09:19:46 +0700

    fix api

--- src/models/User.js
+++ src/models/User.js
@@ -8,14 +8,6 @@ const userSchema = new Schema(
       type: String,
       required: true,
     },
-    role: {
-      type: String,
-      required: true,
-    },
-    curentPlan: {
-      type: String,
-      required: true,
-    },
     email: {
       type: String,
       unique: true,
