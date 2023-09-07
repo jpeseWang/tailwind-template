058d07501fbe108846e4cf9a885f7504aac5d385
wang <quang07112004@gmail.com>
2023-09-07 23:11:59 +0700

    implement animation with  framer motion

--- src/app/community/contribution/page.jsx
+++ src/app/community/contribution/page.jsx
@@ -140,8 +140,40 @@ export default function Contribution() {
                   </div>
                   <h3 className="mt-3 text-lg font-semibold leading-6 text-white">
                     <a href={post.href}>
-                      <span className="absolute inset-0" />
+                      <span className="absolute inset-0  " />
                       {post.title.slice(0, 50)}
+                      <div className="text-base flex justify-between pr-10">
+                        {" "}
+                        <span className="">{post.sourceLang}</span>
+                        <span className="font-medium ">
+                          {post.ratings.length > 0 ? (
+                            <span>
+                              {(
+                                post.ratings.reduce(
+                                  (total, mark) => total + mark.score,
+                                  0
+                                ) / post.ratings.length
+                              ).toFixed(1)}
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth={1.5}
+                                stroke="currentColor"
+                                className="w-4 h-4 inline mb-1"
+                              >
+                                <path
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                  d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
+                                />
+                              </svg>
+                            </span>
+                          ) : (
+                            <span> </span>
+                          )}
+                        </span>
+                      </div>
                     </a>
                   </h3>
                 </article>
